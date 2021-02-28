ae262481303e6cb282d92e13a97ec72d5c1f233b
dennis <travlInfo4you@gmail.com>
2021-02-28 12:25:53 +0900

    어댑터 뼈대작업(2) - getView 내부 초안 완성

--- app/src/main/java/kr/co/tjoeun/jikbanglist_20210228/adapters/RoomAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/jikbanglist_20210228/adapters/RoomAdapter.kt
@@ -1,12 +1,33 @@
 package kr.co.tjoeun.jikbanglist_20210228.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import kr.co.tjoeun.jikbanglist_20210228.R
 import kr.co.tjoeun.jikbanglist_20210228.datas.Room
 
 class RoomAdapter(val mContext : Context,
                   val resId : Int,
                   val mList : ArrayList<Room>) : ArrayAdapter<Room> (mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        // 재활용할 뷰가 있나요???
+        if(tempRow == null) {
+            tempRow = inf.inflate(R.layout.room_list_item, null)
+        }
+
+        // 진짜로 한칸을 그려주자. 절대 null이 아니라는 표시로 '!!' 를 단다.
+        val row = tempRow!!
+
+        return row
+    }
 
 }
 
